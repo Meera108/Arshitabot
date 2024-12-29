39049c5808cfc745039cb84718b7c7ac98d275d8
Meera108 <141446442+Meera108@users.noreply.github.com>
2024-12-29 17:37:25 +0530

    Update video.js

--- Priyansh/commands/video.js
+++ Priyansh/commands/video.js
@@ -9,7 +9,7 @@ module.exports = {
     name: "video",
     version: "1.0.1",
     hasPermssion: 0,
-    credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+    credits: "𝐃𝐞𝐯 𝐕𝐚𝐫𝐝𝐡𝐚𝐧",
     description: "Download YouTube song from keyword search and link",
     commandCategory: "Media",
     usages: "[songName] [type]",
