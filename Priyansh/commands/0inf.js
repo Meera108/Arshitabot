d8c1b3b11f2b280cd60e6a2edb5e80138b5208cf
Meera108 <141446442+Meera108@users.noreply.github.com>
2024-12-29 20:03:16 +0530

    Update 0inf.js

--- Priyansh/commands/0inf.js
+++ Priyansh/commands/0inf.js
@@ -31,7 +31,7 @@ var callback = () => api.sendMessage({body:`🖤 🇮🇳 𝐀𝐃𝐌𝐈𝐍 �
 
 🔥Bot Admin🔥☞︎︎︎☜︎︎︎✰ 𝐃𝐄𝐕 🖤🦋
 
-🙈My owner facebook id link🙈➪ log.messageData{Tag-BotAdmin}
+🙈My owner facebook id link🙈➪ ${global.config.ADMINID}
 
 👋For Any Kind Of Help Contact On Telegram Username 👉 @RohanThakurYT😇
 
