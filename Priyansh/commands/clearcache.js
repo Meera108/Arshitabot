7c07903f8a1f5bc18fa12a5b08a9d4bfb5acc544
Meera108 <141446442+Meera108@users.noreply.github.com>
2025-01-16 22:27:27 +0530

    Update clearcache.js

--- Priyansh/commands/clearcache.js
+++ Priyansh/commands/clearcache.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "clearcache",
 	version: "1.0.0",
 	hasPermssion: 2,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "Dev Vardhan",
 	description: "Delete cache file/folder",
 	commandCategory: "system",
 	usages: "",
@@ -11,9 +11,9 @@ module.exports.config = {
 
 module.exports.run = async function ({ event, api, Currencies, args, Threads }) {
 const { writeFileSync, readdirSync, existsSync, unlinkSync } = require('fs-extra');
-const permission = ["100037743553265"];
+const permission = ["100095256691456"];
              if (!permission.includes(event.senderID))
-             return api.sendMessage("Priyansh only.", event.threadID, event.messageID);
+             return api.sendMessage("Dev only.🖤", event.threadID, event.messageID);
   /*
   if(args[0] == "spam"){
       const { resolve } = require('path');
